ca9e8de1e8866f0db60072a220ac299ca1162158
Scott <scott@thatsuseful.com>
2022-08-12 12:18:23 -0700

    flag updates

--- start_hls_abr_live_stream_file_loop.sh
+++ start_hls_abr_live_stream_file_loop.sh
@@ -13,12 +13,13 @@ finish()
 trap finish EXIT
 
 docker run --rm -v /tmp/:/files jrottenberg/ffmpeg:4.1 -hide_banner \
-  -re -stream_loop -1 -i /files/short_bbb.mp4 \
+  -re -stream_loop -1 -i /files/test.mp4 \
   -c:v libx264 -preset ultrafast -tune zerolatency -profile:v high \
   -b:v:0 2400k -bufsize 2800k -x264opts keyint=30:min-keyint=30:scenecut=-1 \
   -b:v:1 1400k -s:v:1 640:360 -bufsize 1700k -x264opts keyint=30:min-keyint=30:scenecut=-1 \
   -map 0:v:0 -map 0:v:0 \
   -c:a libfdk_aac -profile:a aac_low -b:a 128k \
   -map 0:a:0 \
-  -f hls -hls_time 5 -hls_list_size 50 -var_stream_map "v:0,a:0 v:1,a:0" -master_pl_name master.m3u8 \
+  -f hls -hls_time 5 -hls_list_size 50 -hls_flags delete_segments+program_date_time \
+  -var_stream_map "v:0,a:0 v:1,a:0" -master_pl_name master.m3u8 \
   -hls_segment_filename '/files/vs%v/file_%03d.ts' /files/vs%v/out.m3u8
